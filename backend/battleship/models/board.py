68a8bba19ceb83618e059882f810d180b14bcfea
ionic101 <iv.osipov2004@gmail.com>
2025-01-31 00:42:13 +0300

    Add offset for ships, edit count spawn ships

--- backend/battleship/models/board.py
+++ backend/battleship/models/board.py
@@ -5,14 +5,63 @@ from battleship.models.player import Player
 from random import choice
 
 
-size_to_count_ships: dict[int, int] = {
-    1: 4,
-    2: 3,
-    3: 2,
-    4: 1
+size_to_count_ships: dict[int, dict[int, int]] = {
+    1: {
+        1: 4,
+        2: 3,
+        3: 2,
+        4: 1
+    },
+    2: {
+        1: 4,
+        2: 3,
+        3: 2,
+        4: 1
+    },
+    3: {
+        1: 4,
+        2: 3,
+        3: 2,
+        4: 1
+    },
+    4: {
+        1: 4,
+        2: 3,
+        3: 2,
+        4: 1
+    },
+    5: {
+        1: 3,
+        2: 2,
+        3: 1
+    },
+    6: {
+        1: 3,
+        2: 2,
+        3: 1
+    },
+    7: {
+        1: 3,
+        2: 2,
+        3: 1
+    },
+    8: {
+        1: 3,
+        2: 2,
+        3: 1
+    },
+    9: {
+        1: 2,
+        2: 1
+    },
+    10: {
+        1: 2,
+        2: 1
+    },
 }
 start_coord: Coord = Coord(0, 0)
 end_coord: Coord = Coord(29, 29)
+offset_coords: list[Coord] = [Coord(dx, dy) for dx in range(-1, 2) for dy in range(-1, 2) if not (dx == dy == 0)]
 
 
 class Board:
@@ -20,15 +69,14 @@ class Board:
         self.ships_coords: dict[Coord, Ship] = {}
         self.ships: list[Ship] = []
         self.shots: dict[Coord, ShotType] = {}
+        self.__occupied_coords: set[Coord] = set()
         self.generate_board(players)
     
     def __is_ship_possible(self, coord: Coord, size: int, is_vertical: bool) -> bool:
-        if coord in self.ships_coords:
-            return False
         for d in range(size):
             d_coord: Coord = Coord(0, d) if is_vertical else Coord(d, 0)
             new_coord: Coord = coord + d_coord
-            if new_coord in self.ships_coords or new_coord.x < start_coord.x or new_coord.y < start_coord.y or new_coord.x > end_coord.x or new_coord.y > end_coord.y:
+            if new_coord in self.__occupied_coords or new_coord.x < start_coord.x or new_coord.y < start_coord.y or new_coord.x > end_coord.x or new_coord.y > end_coord.y:
                 return False
         return True
     
@@ -41,18 +89,20 @@ class Board:
             if self.__is_ship_possible(random_coord, size, not is_vertical):
                 return Ship(player, random_coord, size, not is_vertical)
     
-    def __add_coords_shipt_to_board(self, ship: Ship) -> None:
+    def __add_coords_ship_to_board(self, ship: Ship) -> None:
         for d in range(ship.size):
             d_coord: Coord = Coord(0, d) if ship.is_vertical else Coord(d, 0)
             cur_coord: Coord = ship.coord + d_coord
             self.ships_coords[cur_coord] = ship
+            for coord in offset_coords:
+                self.__occupied_coords.add(cur_coord + coord)
     
     def generate_board(self, players: list[Player]):
         for player in players:
-            for size, count in size_to_count_ships.items():
+            for size, count in size_to_count_ships[len(players)].items():
                 for _ in range(count):
                     ship: Ship = self.__generate_ship_in_random(player, size)
-                    self.__add_coords_shipt_to_board(ship)
+                    self.__add_coords_ship_to_board(ship)
                     self.ships.append(ship)
     
     def get_player_ships(self, player: Player) -> list[Ship]:

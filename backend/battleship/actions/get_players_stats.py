336778233751c323ca346f2da6adbecfe3800dda
ionic101 <iv.osipov2004@gmail.com>
2025-02-20 05:19:17 +0500

    Add text of player move. Update text of players stats

--- backend/battleship/actions/get_players_stats.py
+++ backend/battleship/actions/get_players_stats.py
@@ -4,17 +4,21 @@ from battleship.models.game import Game
 
 
 async def action_get_players_stats(websocket: WebSocket, game: Game, **kwargs) -> None:
-    players_to_ships: dict[str, dict[str, int]] = {}
+    players_to_ships: dict[str, dict[str, dict[str, int] | str]] = {}
     for ship in game.board.ships:
         if ship.lifes == 0:
             continue
         if ship.player.username in players_to_ships:
-            players_to_ships[ship.player.username][str(ship.size)] += 1
+            players_to_ships[ship.player.username]['ships'][str(ship.size)] += 1 # type: ignore
         else:
             players_to_ships[ship.player.username] = {
-                "1": 0,
-                "2": 0,
-                "3": 0,
-                "4": 0
+                'color': ship.player.color.value,
+                'ships': {
+                    "1": 0,
+                    "2": 0,
+                    "3": 0,
+                    "4": 0
+                }
             }
+
     await websocket.send_json({'action': 'get_players_stats', 'stats': players_to_ships})
